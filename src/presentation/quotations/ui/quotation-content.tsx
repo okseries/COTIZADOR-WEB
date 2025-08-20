f939a05bd84d37f8121af4b03d475d6d63878051
okseries <okseries211295@gmail.com>
2025-08-20 11:13:04 -0400

    refactor: comment out StoreDebugger import for cleaner code in QuotationContent component

--- src/presentation/quotations/ui/quotation-content.tsx
+++ src/presentation/quotations/ui/quotation-content.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { Stepper } from './stepper/stepper-quotation'
 import { useForm, FormProvider } from "react-hook-form";
-import StoreDebugger from '@/components/debug/StoreDebugger';
+// import StoreDebugger from '@/components/debug/StoreDebugger';
 
 const QuotationContent = () => {
   const methods = useForm();
@@ -11,7 +11,6 @@ const QuotationContent = () => {
     <div className="h-full w-full">
       <FormProvider {...methods}>
         <Stepper/>
-        //! Activar para debugg
         {/* <StoreDebugger /> */}
       </FormProvider>
     </div>
